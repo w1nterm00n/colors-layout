76cbf01dcb7c65b64ccf38575b34adefb720f649
Daria <kordesovad@gmail.com>
2025-07-22 18:39:09 +0200

    added code to calculate the total cart price

--- components/cart/cart-checkout/cart-checkout.js
+++ components/cart/cart-checkout/cart-checkout.js
@@ -1,11 +1,11 @@
-export function renderCartCheckout() {
+export function renderCartCheckout(totalPrice) {
     const template = document.createElement('template');
   
     template.innerHTML = `
         <div class="cart-checkout">
         <div class="cart-checkout__summary">
             <span class="cart-checkout__label">Итого</span>
-            <span class="cart-checkout__price">14&nbsp;400<span class="cart-checkout__currency">₽</span></span>
+            <span class="cart-checkout__price">${totalPrice}<span class="cart-checkout__currency">₽</span></span>
         </div>
         <button class="cart-checkout__button">
             Оформить заказ
--- components/cart/cart.js
+++ components/cart/cart.js
@@ -5,15 +5,11 @@ import { renderCartItem } from "./cart-item/cart-item";
 export function renderCart() {
     const container = document.createElement('div');
     container.classList.add('cart-container');
-    
-
-    const cartHeader = renderCartHeader()
-    const cartCheckout = renderCartCheckout()
-
-    container.appendChild(cartHeader);
-
-    //рендерим товары из local storage
     const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    const cartHeader = renderCartHeader();
+    container.appendChild(cartHeader);
+    //рендерим товары из local storage
     if (cart) {
         cart.forEach(product => {
             const cartItem = renderCartItem(product);
@@ -22,7 +18,16 @@ export function renderCart() {
     }
     //рендерим товары из local storage
 
+    //в cart checkout передаю цену
+    let totalPrice = 0;
+    if (cart.length > 0) {
+        cart.forEach(e => {
+            totalPrice += e.quantity * e.price;
+        });
+    }
+    const cartCheckout = renderCartCheckout(totalPrice);
     container.appendChild(cartCheckout);
+
 
     return container;
   }
