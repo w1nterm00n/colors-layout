fe75d964bc09f4a6b8daf831c88465fdcf69586b
Daria <kordesovad@gmail.com>
2025-07-23 10:00:53 +0200

    added display of categories in sidebar-filters

--- components/sidebar-filters/sidebar-filters.js
+++ components/sidebar-filters/sidebar-filters.js
@@ -1,48 +1,31 @@
+import { getProducts } from "../../utils/storage";
+
 export function renderSidebarFilters() {
     const template = document.createElement('template');
   
+    let products = getProducts();
+    let categories = new Set();
+    products.forEach(e => {
+        categories.add(e.category)
+    });
+
+    // Генерация HTML-фильтров на основе категорий
+    let categoryFiltersHTML = '';
+    categories.forEach(category => {
+        categoryFiltersHTML += `
+            <div class="sidebar-filters__toggle-wrapper">
+                <label class="sidebar-filters__toggle">
+                    <input type="checkbox">
+                    <span class="sidebar-filters__switcher"></span>
+                </label>
+                <p class="sidebar-filters__label">${category}</p>
+            </div>
+        `;
+    });
+
     template.innerHTML = `
         <div class="sidebar-filters sidebar-filters-hidden">
-            <div class="sidebar-filters__toggle-wrapper">
-                <label class="sidebar-filters__toggle">
-                    <input type="checkbox">
-                    <span class="sidebar-filters__switcher"></span>
-                </label>
-                <p class="sidebar-filters__label">Новинки</p>
-            </div>
-
-            <div class="sidebar-filters__toggle-wrapper">
-                <label class="sidebar-filters__toggle">
-                    <input type="checkbox">
-                    <span class="sidebar-filters__switcher"></span>
-                </label>
-                <p class="sidebar-filters__label">Есть в наличии</p>
-            </div>
-            
-            <div class="sidebar-filters__toggle-wrapper">
-                <label class="sidebar-filters__toggle">
-                    <input type="checkbox">
-                    <span class="sidebar-filters__switcher"></span>
-                </label>
-                <p class="sidebar-filters__label">Контрактные</p>
-            </div>
-
-            <div class="sidebar-filters__toggle-wrapper">
-                <label class="sidebar-filters__toggle">
-                    <input type="checkbox">
-                    <span class="sidebar-filters__switcher"></span>
-                </label>
-                <p class="sidebar-filters__label">Эксклюзивные</p>
-            </div>
-
-
-            <div class="sidebar-filters__toggle-wrapper">
-                <label class="sidebar-filters__toggle">
-                    <input type="checkbox">
-                    <span class="sidebar-filters__switcher"></span>
-                </label>
-                <p class="sidebar-filters__label">Распродажа</p>
-            </div>
+            ${categoryFiltersHTML}
         </div>
     `.trim();
   
